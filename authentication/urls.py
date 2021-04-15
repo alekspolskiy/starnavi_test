64ac95ba2a7aac167b4c9dfac1b85dc6028a69a1
Alex Polskyi <alekspolskyi@gmail.com>
2021-04-15 06:20:45 +0300

    Add refresh token view

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import SignUpView, LoginView, UsersListView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view()),
     path('login/', LoginView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('users/', UsersListView.as_view()),
 ]
